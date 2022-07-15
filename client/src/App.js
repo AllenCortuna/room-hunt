20f1dd4cd463e976f7a4fc42c50c1e20ba090893
zanz <olsencortuna@gmail.com>
2022-07-15 15:29:57 +0800

    ta*lw(

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,20 @@
  
  
-import { CssBaseline } from "@mui/material";
-import { ThemeProvider } from "@mui/material/styles";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./component/Home";
 import Navbar from "./component/Navbar";
-import theme from "./theme.js";
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
+    // <ThemeProvider theme={theme}>
+    //   <CssBaseline />
       <Router>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
       </Router>
-    </ThemeProvider>
+    // </ThemeProvider>
   );
 };
 export default App;
